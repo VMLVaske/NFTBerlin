bf213433a726ff31aa65ec7575b84836dbc3d258
Valerie <valerie.vaske@gmail.com>
2022-05-27 22:35:53 +0200

    tmp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,18 +3,12 @@ import { Button, Stack } from 'react-bootstrap';
 import axios from 'axios';
 
 import { useState } from 'react'
-import Arweave from 'arweave'
-
-/* connect to an Arweave node or specify a gateway  */
-const arweave = Arweave.init({
-  host: 'arweave.net',
-})
 
 function App() {
   const [state, setState] = useState('')
   const [transactionId, setTransactionId] = useState('')
 
-  async function streamData() {
+  async function getStream() {
 
     axios.get(`https://livepeer.com/api/stream/979e07b7-9928-4e4f-abdf-f141b184c5b5`,
       { headers: { authorization: 'Bearer a0e86a1b-b8f2-4134-bbd4-67cb0155159d' } }).then(response => {
@@ -24,7 +18,31 @@ function App() {
       .catch((error) => {
         console.log('error ' + error);
       });
-    } 
+  }
+
+  async function getSessionID() {
+
+    axios.get(`https://livepeer.com/api/stream/979e07b7-9928-4e4f-abdf-f141b184c5b5/sessions`,
+      { headers: { authorization: 'Bearer a0e86a1b-b8f2-4134-bbd4-67cb0155159d' } }).then(response => {
+        // If request is good...
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log('error ' + error);
+      });
+  }
+
+  async function getSession() {
+
+    axios.get(`https://livepeer.com/api/stream/979e07b7-9928-4e4f-abdf-f141b184c5b5/sessions`,
+      { headers: { authorization: 'Bearer a0e86a1b-b8f2-4134-bbd4-67cb0155159d' } }).then(response => {
+        // If request is good...
+        console.log(response.data[0]);
+      })
+      .catch((error) => {
+        console.log('error ' + error);
+      });
+  }
 
   return (
     <div>
@@ -33,8 +51,15 @@ function App() {
         <Button>
           Connect to Wallet
         </Button>
-        <Button onClick={streamData}>
-          Get Livestream
+        <Button onClick={getStream}>
+          Get Stream
+        </Button>
+        <Button onClick={getSessionID}>
+          Get SessionID
+        </Button>
+
+        <Button onClick={getSession}>
+          Get Stream
         </Button>
       </Stack>
     </div>
