0628b2db75739898b1e525ae8273963446ca3189
Kurt <Kurt@Merbeth.io>
2022-05-27 21:17:46 +0200

    express dinge

--- server/index.js
+++ server/index.js
@@ -1,14 +1,48 @@
-const express = require('express');
-const app = express();
+const express = require("express")
+const app = express()
+const axios = require('axios');
+var cors = require('cors')
+
+require('dotenv').config()
+
+app.use(cors())
+
 const port = process.env.PORT || 4999;
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
-});
+
+// --------- run npm install first :) ---------
+
+// --------- internal functions ---------
+
+const queryLivepeer = async () => {
+  let result;
+  await axios.get(`https://livepeer.com/api/stream/979e07b7-9928-4e4f-abdf-f141b184c5b5`,
+  { headers: { authorization: 'Bearer a0e86a1b-b8f2-4134-bbd4-67cb0155159d' } }).then(response => {
+    // If request is good...
+    result = response.data;
+  })
+  .catch((error) => {
+    result = 'error ' + error;
+  });
+
+  return result;
+}
+
+// --------- API functions ---------
+
+// https://localhost:4999/get/livepeerData
+app.get("/get/livepeerData", (req, res) => {
+ queryLivepeer().then(
+   result => {
+     res.send(result)
+   }
+ ).catch( 
+   err => { 
+     res.send(err)
+    }
+  )
+})
+
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
--- server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-router.get('https://livepeer.com/api/stream/979e07b7-9928-4e4f-abdf-f141b184c5b5', (req, res) => {
-    console.log("Req: "); 
-    console.log(req); 
-    console.log("Res: "); 
-    console.log(res); 
-});
-module.exports = router;
